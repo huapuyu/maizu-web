effd39b3b3088ad30c334520008749d29b8c579c
huapuyu <huapuyu@yahoo.com.cn>
2011-11-30 17:33:18 +0800

    submit

--- src/main/java/com/anders/maizu/action/shh/readme.txt
+++
@@ -1,2 +0,0 @@
-二手房
-SecondHandHouse
--- src/main/java/com/anders/maizu/action/skeleton/Token.java
+++
@@ -1,8 +0,0 @@
-package com.anders.maizu.action.skeleton;
-
-import com.anders.maizu.action.BaseAction;
-
-public class Token extends BaseAction {
-
-	private static final long serialVersionUID = 1L;
-}
